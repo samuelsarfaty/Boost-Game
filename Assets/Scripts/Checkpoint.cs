aea441632ef9ed21e67d0f785042bd976a02d042
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-11-18 18:46:57 +0000

    experimenting with dolly tracking, need to look into switiching cameras

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Checkpoint : MonoBehaviour {
+
+	[SerializeField] CinemachineVirtualCamera virtualCamera;
+	[SerializeField] float dollyTargetPosition;
+	private CinemachineTrackedDolly trackedDolly;
+
+	void Awake(){
+		trackedDolly = virtualCamera.GetCinemachineComponent<CinemachineTrackedDolly> ();
+	}
+
+	void OnCollisionEnter(Collision other){
+		if (other.gameObject.tag == "Player"){
+			FixCamera (dollyTargetPosition);
+		}
+	}
+
+	void FixCamera(float targetPosition){
+		transform.rotation = Quaternion.identity;
+		trackedDolly.m_AutoDolly.m_Enabled = false;
+		virtualCamera.Follow = null;
+		virtualCamera.LookAt = null;
+		trackedDolly.m_PathPosition = targetPosition;
+
+	}
+}
