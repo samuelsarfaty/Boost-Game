bdd9ece288715139d49d21124a26ac859685c5cc
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2018-12-15 17:42:08 +0000

    block out environment

---
+++ Assets/Scripts/MoveWithCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithCamera : MonoBehaviour {
+
+	public Transform target;
+
+	void FixedUpdate(){
+		transform.LookAt (target);
+	}
+}
