80a8926ee486f07df446ee5af2e09ced99fa319b
Samuel Sarfaty <samuel.sarfaty@gmail.com>
2019-01-06 17:07:46 +0000

    Added another level + expanded rotator

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Rotator : MonoBehaviour {
 
-	[SerializeField] Vector3 rotationVector = Vector3.zero;
+	[SerializeField] bool rotateAroundObject = false;
+	[Tooltip("Add target if you want to rotate around object")]
+	[SerializeField] Transform target = null;
+	//[SerializeField] Vector3 rotationVector = Vector3.zero;
+	[SerializeField] Vector3 rotationAxis = Vector3.zero;
 	[SerializeField] float speed = 0f;
 	
 	// Update is called once per frame
 	void Update () {
-		Rotate ();
+
+		if(rotateAroundObject){
+			RotateAround (target.position, rotationAxis, speed);
+		} else {
+			Rotate ();	
+		}
 	}
 
 	void Rotate(){
-		transform.Rotate (rotationVector, speed * Time.deltaTime);
+		transform.Rotate (rotationAxis, speed * Time.deltaTime);
+	}
+
+	void RotateAround(Vector3 target, Vector3 axis, float speed){
+		transform.RotateAround (target, axis, speed * Time.deltaTime);
 	}
 }
